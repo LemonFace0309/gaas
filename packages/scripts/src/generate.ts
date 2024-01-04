ebb079b17560ad1ae0b43778755093448206e4a2
LemonFace0309 <charles.liu0309@gmail.com>
2024-01-04 17:57:42 +0800

    now generates novel images

--- packages/scripts/src/generate.ts
+++ packages/scripts/src/generate.ts
@@ -1,10 +1,21 @@
 import fs from "fs";
 import path from "path";
 import { zip } from "lodash";
+import axios from "axios";
 
-const imgDir = path.join(__dirname, "..", ".images");
+const sourceDir = path.join(__dirname, "..", ".images");
+const outputDir = path.join(__dirname, "..", ".generated");
 
-// const API = `http://127.0.0.1:7860/sdapi/v1`
+const PROMPT_SUFFIX =
+  "head tilt, (looking at viewer:1.5), complex, korean, (large breasts:1.5), detailed background, dramatic lighting";
+const NEGATIVE_PROMT =
+  "badhandv4, paintings, sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, age spot, manboobs, double navel, muted arms, fused arms, analog, analog effects, bad architecture, watermark, (mole:1.5), EasyNegative";
+
+const API = `http://127.0.0.1:7860/sdapi/v1`;
+
+const client = axios.create({
+  baseURL: API,
+});
 
 function imageToBase64(filePath: string): string {
   const bitmap = fs.readFileSync(filePath);
@@ -14,23 +25,27 @@ function imageToBase64(filePath: string): string {
 function getAccounts() {
   let accounts: string[] = [];
 
-  if (fs.existsSync(imgDir)) {
-    const contents = fs.readdirSync(imgDir);
+  if (fs.existsSync(sourceDir)) {
+    const contents = fs.readdirSync(sourceDir);
 
     for (const f of contents) {
-      const fullPath = path.join(imgDir, f);
+      const fullPath = path.join(sourceDir, f);
       const stat = fs.statSync(fullPath);
 
       if (stat.isDirectory()) {
         // Recursively process subdirectories
-        accounts.push(fullPath);
+        accounts.push(f);
       }
     }
   } else {
-    console.log("Directory not found:", imgDir);
+    console.log("Directory not found:", sourceDir);
   }
 
   return accounts;
+}
+
+function getAccountDir(account: string): string {
+  return path.join(sourceDir, account);
 }
 
 function getImages(dir: string): string[] {
@@ -55,20 +70,111 @@ function getImages(dir: string): string[] {
   return base64Images;
 }
 
-function getClip(imgBase64: string) {
-  return imgBase64 
+async function getPrompt(imgBase64: string) {
+  try {
+    const res = await client.post<{
+      caption: string;
+    }>("/interrogate", {
+      image: imgBase64,
+      model: "clip",
+    });
+    const clip = res.data.caption + PROMPT_SUFFIX;
+    return `${clip}`;
+  } catch (err) {
+    console.log(`Unable to obtain clip for this image: ${err}`);
+  }
+  return null;
 }
 
-function main() {
+async function getPrompts(account: string, imgs: string[]): Promise<string[]> {
+  console.log(`Starting to generate prompts for ${account}`)
+
+  let prompts: string[] = [];
+
+  let idx = 1
+  for (const img of imgs) {
+    console.log(`Generating prompt #${idx}`);
+    let prompt = await getPrompt(img);
+
+    if (!prompt) {
+      console.log(`No prompt was returned for ${idx}. Defaulting to ""`);
+      prompt = "";
+    }
+
+    prompts.push(prompt);
+    idx += 1
+  }
+
+  console.log(`Done generating prompts for ${account}`)
+  return prompts;
+}
+
+async function generateImg(img: string, prompt: string) {
+  try {
+    const res = await client.post<{
+      images: string[];
+    }>("/img2img", {
+      prompt,
+      negative_prompt: NEGATIVE_PROMT,
+      init_images: [img],
+    });
+    return res.data.images[0];
+  } catch (err) {
+    console.log(`Unable to generate image: ${err}`);
+  }
+  return null;
+}
+
+async function generateImgs(
+  account: string,
+  imgs: string[],
+  prompts: string[]
+) {
+  console.log(`Starting to generate images fro ${account}`)
+  const imgClipPairs = zip(imgs, prompts);
+
+  let idx = 1
+  for (const [img, prompt] of imgClipPairs) {
+    if (!img || !prompt) {
+      continue;
+    }
+
+    console.log("prompt:", prompt);
+
+    console.log(`Generating image #${idx}`);
+    const generatedImg = await generateImg(img, prompt);
+
+    if (!generatedImg) {
+      console.log(`No image was returned for ${idx}`);
+      continue;
+    }
+
+    const buffer = Buffer.from(generatedImg, "base64");
+
+    const generatedImgDir = path.join(outputDir, account);
+
+    if (!fs.existsSync(generatedImgDir)) {
+      fs.mkdirSync(generatedImgDir, { recursive: true });
+    }
+    const generatedImgPath = path.join(generatedImgDir, `${idx}.jpg`);
+
+    fs.writeFileSync(generatedImgPath, buffer);
+    idx++;
+  }
+
+  console.log(`Done generating images for ${account}`);
+}
+
+async function main() {
   const accounts = getAccounts();
 
   for (const account of accounts) {
-    const images = getImages(account);
-    const clips = images.map((img) => getClip(img))
-
-    const imgPackage = zip(images, clips)
-    console.log(imgPackage)
+    const accountDir = getAccountDir(account);
+    const images = getImages(accountDir).slice(0, 1);
+    const prompts = await getPrompts(account, images); 
+    
+    await generateImgs(account, images, prompts);
   }
 }
 
-main()
+main();
